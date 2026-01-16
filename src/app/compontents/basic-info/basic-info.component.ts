6a8152c16b386a12481a8f075d97f378bf6e0ece
bart <cieslabartek721@gmail.com>
2026-01-16 19:38:19 +0100

    creating and styling of the first section

---
+++ src/app/compontents/basic-info/basic-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-info',
+  imports: [],
+  templateUrl: './basic-info.component.html',
+  styleUrl: './basic-info.component.css'
+})
+export class BasicInfoComponent {
+
+}

354d3d7e21bb748e9e326ce8a345f6bd89a57b75
bart <cieslabartek721@gmail.com>
2026-01-17 23:09:19 +0100

    Fitting everything together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
-import { BasicInfoComponent } from './compontents/basic-info/basic-info.component';
+import { BasicInfoComponent } from './components/basic-info/basic-info.component';
+import { AbilityScoresComponent } from './components/ability-scores/ability-scores.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { MovementComponent } from './components/movement/movement.component';
+import { ArmorComponent } from './components/armor/armor.component';
+import { HealthComponent } from './components/health/health.component';
+import { SensesComponent } from './components/senses/senses.component';
+import { AttacksComponent } from './components/attacks/attacks.component';
+import { EquipmentComponent } from './components/equipment/equipment.component';
+import { ProficienciesComponent } from './components/proficiencies/proficiencies.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BasicInfoComponent],
+  imports: [
+    BasicInfoComponent, 
+    AbilityScoresComponent, 
+    SkillsComponent,
+    MovementComponent,
+    ArmorComponent,
+    HealthComponent,
+    SensesComponent,
+    AttacksComponent,
+    EquipmentComponent,
+    ProficienciesComponent
+  ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
